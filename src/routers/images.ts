56fd525dfc878584cadeea2545663397014495c0
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-06-12 11:30:58 +0200

    routes added

--- src/routers/images.ts
+++ src/routers/images.ts
@@ -1,9 +1,53 @@
-import express, {Router, Request, Response} from 'express';
+import express, { Router, Request, Response } from 'express';
+import { getImagePath, getThumbnailPath } from './../image-processing/paths';
+import { setImageUrl } from './../image-processing/index';
+import Image from './../image-processing/types/image';
 
-const routes:Router = express.Router(); 
+const routes: Router = express.Router();
 
-routes.get('/', (request:Request, response:Response):void=>{
-    response.status(200).send('Routes work well!');
-});
+const validateImage = (width: number, height: number): boolean => {
+  if (Number.isNaN(width) || Number.isNaN(height)) {
+    return false;
+  }
+  return true;
+};
 
-export default routes; 
+const handleApi = async (
+  request: Request,
+  response: Response
+): Promise<void> => {
+  if (request.query.imageName) {
+    if (request.query.height || request.query.width) {
+      let valid = validateImage(
+        parseInt(request.query.width as string),
+        parseInt(request.query.height as string)
+      );
+      if (valid) {
+        let thumbnail = await getThumbnailPath(request.query);
+        if (thumbnail) {
+          response.sendFile(thumbnail);
+        } else {
+          let newThumbnail = await setImageUrl(request.query);
+          if (newThumbnail) {
+            response.sendFile(newThumbnail);
+          } else {
+            response.status(200).json({ Message: 'Thumbnail not Found' });
+          }
+        }
+      }
+    } else {
+      const image = await getImagePath(request.query.imageName as Image);
+      if (image) {
+        response.sendFile(image);
+      } else {
+        response.status(200).json({ Message: 'Image is not valid' });
+      }
+    }
+  } else {
+    response.status(404).json({ Message: "Image doesn't existed" });
+  }
+};
+
+routes.get('/', handleApi);
+
+export default routes;
