88b4522b2e05b0eae62d315a34a85f71a40e3beb
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-06-12 09:13:33 +0200

    paths added

--- src/image-processing/index.ts
+++ src/image-processing/index.ts
@@ -1,3 +1,3 @@
 import Image from './types/image';
 import ImageResize from './image-resize';
-
+import {thumbnailDirectory, imageDirectory} from './paths';
---
+++ src/image-processing/paths/index.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export const thumbnailDirectory = path.resolve(__dirname, './../../../assets/images/thumbnails');
+export const imageDirectory = path.resolve(__dirname, './../../../assets/images/full');
+
